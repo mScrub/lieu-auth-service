136f333a602c293416f051fce34e8d316f038923
Felix <flieu@my.bcit.ca>
2023-11-15 12:18:14 -0800

    refactor: changed url encoder to json()

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
